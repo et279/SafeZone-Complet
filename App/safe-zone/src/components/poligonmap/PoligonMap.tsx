d3a421e74d6211b022306ea59ecd74da64742180
FabianBeltran <fabianbeltranrojas@hotmail.com>
2024-07-09 01:33:52 -0500

    Actualizacion con error
    Se agregaron try para permitir el funcionamiento de las funciones correctas, falta poder mostrar las zonas permitidas

--- App/safe-zone/src/components/poligonmap/PoligonMap.tsx
+++ App/safe-zone/src/components/poligonmap/PoligonMap.tsx
@@ -1,41 +1,86 @@
 import React, { useEffect, useState } from 'react';
-import { Polygon, Popup} from 'react-leaflet';
+import { Polygon, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import { fetchZones, generatePolygons } from '../../types/polygonLogic';
-import { Restriction, Zone, } from '../../types/Zone';
-
+import { fetchZones, generatePolygons, unifyPolygons} from '../../types/polygonLogic';
+import { Restriction, Zone } from '../../types/Zone';
 import './PoligonMap.css';
-
+import { POLYGON_ENVIGADO } from '../../types/Variables'
 
 const PolygonMap: React.FC = () => {
-  const [polygons, setPolygons] = useState<{ name: string; coordinates: number[][];    restriction: Restriction;  coordinatesrestriction: number[][] }[]>([]);
+  const [polygons, setPolygons] = useState<{ name: string; coordinates: number[][]; restriction: Restriction; coordinatesrestriction: number[][] }[]>([]);
+  const [unifiedPolygon, setUnifiedPolygon] = useState<number[][]>([]);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const loadPolygons = async () => {
-      const zones: Zone[] = await fetchZones();
-      const generatedPolygons = generatePolygons(zones);
-      setPolygons(generatedPolygons);
-      //console.log(zones);
+      try {
+        const zones: Zone[] = await fetchZones();
+        const generatedPolygons = generatePolygons(zones);
+        setPolygons(generatedPolygons);
+        console.log('genero poligonos');
+      
+        
+        const unified = unifyPolygons(zones);
+        
+        setUnifiedPolygon(unified);
+
+        console.log('completo todo');
+      } catch (error) {
+        console.error('Error loading polygons:', error);
+        setError('An error occurred while loading polygons.');
+      }
     };
 
     loadPolygons();
   }, []);
-//   console.log('poligons',polygons);
+// Definir los puntos del polígono con huecos
+const polygonWithHoles = [
+  [
+    [6.1889, -75.5832474], // Contorno exterior
+    [6.1889801, -75.5833107],
+    [6.1886445, -75.583491],
+    [6.1883309, -75.583671],
+    [6.188069, -75.5838719],
+    [6.1876799, -75.5842998],
+    [6.1871288, -75.5850199],
+    [6.1867403, -75.5855474],
+    [6.1861331, -75.5863644],
+    [6.185522, -75.5872179],
+    [6.1853345, -75.5874469],
+    [6.1889, -75.5832474], // Cerrar el contorno exterior
+  ],
+  [
+    [6.1885, -75.5840], // Hueco interior
+    [6.1886, -75.5841],
+    [6.1887, -75.5842],
+    [6.1885, -75.5840], // Cerrar el hueco interior
+  ],
+];
+console.log(polygonWithHoles);
+console.log(unifiedPolygon);
 
 
   return (
-          polygons.map((polygon, index) => (
-            <React.Fragment key={index}>
-                <Polygon positions={polygon.coordinatesrestriction} className='zone-protect' />
-                <Polygon key={index} positions={polygon.coordinates} className='site-protect'>
-                    <Popup>{polygon.name}
-                    <br>
-                    </br></Popup>   
-                </Polygon>
-            </React.Fragment>
-          ))
-          
+    <div>
+      {polygons.map((polygon, index) => (
+        <React.Fragment key={index}>
+          <Polygon positions={polygon.coordinatesrestriction} className='zone-protect' />
+          <Polygon positions={polygon.coordinates} className='site-protect'>
+            <Popup>{polygon.name}</Popup>
+          </Polygon>
+        </React.Fragment>
+      ))}
+      {unifiedPolygon.length > 0 && (
+        <Polygon positions={unifiedPolygon} className='unified-zone' />
+      )}
+      {error && <div className="error-message">{error}</div>}
+      <Polygon positions={polygonWithHoles} color="blue" />
+      {/* <React.Fragment >
+        <Polygon positions={POLYGON_ENVIGADO} className='city-limit'>
+        </Polygon>
+      </React.Fragment> */}
       
+    </div>
   );
 };
 
--- App/safe-zone/src/types/polygonLogic.ts
+++ App/safe-zone/src/types/polygonLogic.ts
@@ -1,27 +1,59 @@
-import axios from 'axios';
+import * as turf from '@turf/turf';
+import { Feature, GeoJsonProperties, Polygon as GeoJsonPolygon, MultiPolygon as GeoJsonMultiPolygon } from 'geojson';
 import { Zone } from './Zone';
+import api from '../services/api';
+import { POLYGON_ENVIGADO } from '../types/Variables'
 
-
+// Función para obtener las zonas desde la base de datos
 export const fetchZones = async (): Promise<Zone[]> => {
   try {
-    const response = await axios.get('http://localhost:3000/zones');
+    const response = await api.get('zones');
     return response.data;
   } catch (error) {
     console.error('Error fetching zones:', error);
     return [];
   }
 };
-
+// Función para generar polígonos a partir de las zonas
 export const generatePolygons = (zones: Zone[]) => {
-  return zones.map(zone => {
-    const perimeterCoordinates = zone.coordinatesrestriction.map(coord => [coord.lat, coord.lng] as [number, number]);
-    const siteCoordinates = zone.coordinates.map(coord => [coord.lat, coord.lng] as [number, number]);
-    return {
-      name: zone.name,
-      coordinates: siteCoordinates,
-      restriction: zone.restriction,
-      coordinatesrestriction: perimeterCoordinates
+  return zones.map(zone => ({
+    name: zone.name,
+    coordinates: zone.coordinates.map(coord => [coord.lat, coord.lng] as [number, number]),
+    restriction: zone.restriction,
+    coordinatesrestriction: zone.coordinatesrestriction.map(coord => [coord.lat, coord.lng] as [number, number])
+  }));
+};
 
-    };
-  });
+
+// Función para unir los polígonos superpuestos
+export const unifyPolygons = (zones: Zone[]) => {
+  const turfPolygons = zones.map(zone =>
+    turf.polygon([zone.coordinatesrestriction.map(coord => [coord.lng, coord.lat])])
+  );
+  // Convertir POLYGON_ENVIGADO a Feature<GeoJsonPolygon>
+  const containerPolygon: Feature<GeoJsonPolygon, GeoJsonProperties> = turf.polygon([POLYGON_ENVIGADO]);
+
+  // Convertir turfPolygons a Features<GeoJsonPolygon>
+  const turfPolyFeatures: Feature<GeoJsonPolygon, GeoJsonProperties>[] = turfPolygons.map(polygon =>
+    turf.polygon(polygon.geometry.coordinates)
+  );
+let x = turfPolyFeatures[0]
+  // console.log(  turfPolyFeatures  );
+  // Crear un MultiPolygon con el contenedor y los polígonos adicionales
+  const allPolygons: Feature<GeoJsonMultiPolygon, GeoJsonProperties> = turf.multiPolygon([
+    containerPolygon.geometry.coordinates,
+    x.geometry.coordinates
+  ]); 
+  // console.log(  allPolygons.geometry.coordinates );
+  
+  let unifiedPolygon;
+
+  // Si es MultiPolygon, obtener las coordenadas del primer polígono
+  if (allPolygons.geometry.type === 'MultiPolygon') {
+    unifiedPolygon = allPolygons.geometry.coordinates;
+  }
+
+  // console.log(unifiedPolygon);
+  
+  return unifiedPolygon;
 };
